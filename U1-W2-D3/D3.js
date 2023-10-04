f7d056bf2355fc4f63c3d0008a052954e1e867ee
Laura Ceragioli <cera.laura98@gmail.com>
2023-10-04 17:54:54 +0200

    es 5

--- U1-W2-D3/D3.js
+++ U1-W2-D3/D3.js
@@ -146,12 +146,12 @@ console.log(femaleCharacters)
   Ad ognuna di queste proprietà assegna come valore un array vuoto.
 */
 
-const eyecolor = {
+const eyeColor = {
   blue: [] ,
   yellow: [] ,
   brown: [] ,
   red: [] ,
-  bluegray: [] ,
+  "blue-gray": [] ,
 }
 
 
@@ -159,7 +159,23 @@ const eyecolor = {
   Utilizza uno switch statement per inserire uno ad uno gli oggetti dei personaggi di "starWarsCharacters" negli array relativi al colore degli occhi precedentemente creati.
   Ogni personaggio dovrà finire nell'array corrispondente al suo colore degli occhi (al valore della sua proprietà "eye_color").
 */
+for ( let i = 0 ; i < starWarsCharacters.length ; i++) {
+  switch (starWarsCharacters[i].eye_color) {
+  case "blue": eyeColor.blue.push(starWarsCharacters[i])
+  break;
+  case "yellow": eyeColor.yellow.push(starWarsCharacters[i])
+  break;
+  case "brown": eyeColor.brown.push(starWarsCharacters[i])
+  break;
+  case "red": eyeColor.red.push(starWarsCharacters[i])
+  break;
+  case "blue-gray": eyeColor["blue-gray"].push(starWarsCharacters[i])
+  break;
+}
+}
 
+
+console.log(eyeColor)
 
 /* ESERCIZIO 6
   Usa un while loop per calcolare la massa totale dell'equipaggio. Salvala in una variabile chiamata "crewMass".
