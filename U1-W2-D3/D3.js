eae97a4b2e373f859d8fb0411734620c0b04ec94
Laura Ceragioli <cera.laura98@gmail.com>
2023-10-04 18:04:15 +0200

    es 5

--- U1-W2-D3/D3.js
+++ U1-W2-D3/D3.js
@@ -227,6 +227,7 @@ console.log(starWarsCharacters)
   Una volta fatto crea un console.log per controllare la proprietà length di "characters" prima e dopo l'operazione.
 */
 
+
 /* --EXTRA-- ESERCIZIO 10
   Crea una funzionalità che selezioni un elemento casuale dall'array "starWarsCharacters" e ne stampi in console le proprietà in modo discorsivo (a tuo piacimento).
 */
